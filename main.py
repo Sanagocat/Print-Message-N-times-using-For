f6340df34d4aa80864c0d9cb8396404f93847a80
Jefferybazoz <105789022+Sanagocat@users.noreply.github.com>
2022-06-01 11:39:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#get repeat number from User
+#Print Message repeatNumber times (ex. Hello~!! - repeat number : X )
+#Using for i in range(n)
+counting=int(input("How many times? : "))
+
+for i in range(counting): # i >> 0 ~ counting-1
+  print(str(i+1)+" hello")
